4274fd77f6c00036103638f2551d258fc09d6333
Nicolo <nicolorizzi1996@gmail.com>
2020-04-01 18:27:43 +0200

    AGGIUNTA COMMENTI

--- js/main.js
+++ js/main.js
@@ -5,17 +5,24 @@ per i multipli di 5 stampi Buzz.
 per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
  */
 
- // CREO UN CICLO FOR CHE STAMPI I NUMERI CON CONTATORE INIZIALIZZATO A 0 E VADA FINO A 99
+ // CREO UN CICLO FOR CHE STAMPI I NUMERI CON CONTATORE INIZIALIZZATO A 1 E VADA FINO A 100
 for (var i = 1; i <= 100; i++) {
-    
-    // CREO UNA VARIABILE CHE MEMORIZZI AD OGNI CICLO IL VALORE 
+
+    // SE DALLA DIVISIONE CON 3 E 5 OTTENGO IN ENTRAMBI I CASI IL MODULO = 0 ALLORA STAMPA "FIZZBUZZ"
 
     if (i % 3 == 0 && i % 5 == 0) {
-        console.log('FizzBuzz: ' + i)  
+        console.log('FizzBuzz') 
+
+        // ALTRIMENTI SE DALLA DIVISIONE CON 5 OTTENGO MODULO = 0 ALLORA STAMPO "BUZZ"
+
     } else if  (i % 5 == 0) {
-        console.log('Buzz: ' + i);
+        console.log('Buzz');
+        // ALTRIMENTI SE DALLA DIVISIONE CON 3 OTTENGO MODULO = 0 ALLORA STAMPO "FIZZ"
+
     } else if ( i % 3 == 0) {
         console.log('Fizz');
+        
+        // ALTRIMENTI STAMPA SOLO IL VALORE
     } else {
         console.log(i);
         
