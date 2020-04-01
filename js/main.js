0fa4d9f13e846b95fe0e9b061be5f524c599390a
Nicolo <nicolorizzi1996@gmail.com>
2020-04-01 19:11:14 +0200

    PRINTING IN HTML

--- js/main.js
+++ js/main.js
@@ -5,27 +5,36 @@ per i multipli di 5 stampi Buzz.
 per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
  */
 
- // CREO UN CICLO FOR CHE STAMPI I NUMERI CON CONTATORE INIZIALIZZATO A 1 E VADA FINO A 100
-for (var i = 1; i <= 100; i++) {
 
+var puntatore = document.getElementById('lista');
+var valStampare = '';
+// CREO UN CICLO FOR CHE STAMPI I NUMERI CON CONTATORE INIZIALIZZATO A 1 E VADA FINO A 100
+for (var i = 1; i <= 100; i++) {
+    var numero = i;
     // SE DALLA DIVISIONE CON 3 E 5 OTTENGO IN ENTRAMBI I CASI IL MODULO = 0 ALLORA STAMPA "FIZZBUZZ"
 
     if (i % 3 == 0 && i % 5 == 0) {
-        console.log('FizzBuzz') 
+        console.log('FizzBuzz');
+        valStampare += '<li>' + 'FizzBuzz' + '</li>';
+        
 
         // ALTRIMENTI SE DALLA DIVISIONE CON 5 OTTENGO MODULO = 0 ALLORA STAMPO "BUZZ"
 
     } else if  (i % 5 == 0) {
         console.log('Buzz');
+        valStampare += '<li>' + 'Buzz' + '</li>';
+        
         // ALTRIMENTI SE DALLA DIVISIONE CON 3 OTTENGO MODULO = 0 ALLORA STAMPO "FIZZ"
 
     } else if ( i % 3 == 0) {
         console.log('Fizz');
-        
+        valStampare += '<li>' + 'Fizz' + '</li>';
+
         // ALTRIMENTI STAMPA SOLO IL VALORE
     } else {
         console.log(i);
-        
+        valStampare += '<li>' + i + '</li>';
     }
     
 }
+puntatore.innerHTML = valStampare;
